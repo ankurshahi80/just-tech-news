ddc522d92478347759960613278b84cc8119738d
Ankur Shahi <ankurshahi@MacBook-Air.phub.net.cable.rogers.com>
2021-07-06 18:28:32 -0400

    set up sequelize and api routes

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const routes = require('./routes');
+// import the connection to sequelize
+const sequelize = require('./config/connection');
+
+// set up express app
+const app = express();
+const PORT = process.env.PORT || 3001
+
+// middleware
+// parses incoming requests with JSON payloads and return an object
+app.use(express.json());
+// Parses urlencoded bodies and only looks at requests where the Content-Type header matches the type option
+app.use(express.urlencoded({extended: true}));
+
+//turn on routes
+app.use(routes);
+
+//turn on connection to db and server
+sequelize.sync({force:false}).then(()=>{
+    // listen for requests
+    app.listen(PORT,()=> console.log('Now listening'));
+})
