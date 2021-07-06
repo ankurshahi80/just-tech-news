5ab967ba8183e528a20697f06d7480ff4290d822
Ankur Shahi <ankurshahi@MacBook-Air.phub.net.cable.rogers.com>
2021-07-06 18:40:46 -0400

    minor changes

--- routes/Index.js
+++ routes/Index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+// collect the packaged group of API endpoints and prefix them with the `/api` path. 
 router.use('/api',apiRoutes);
 
+// Returns a 404 if a request to made to a non existing endpoint
 router.use((req,res)=> {
     res.status(404).end()
 });
