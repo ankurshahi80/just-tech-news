3217f6178f7bd6d780bea0a8bb780d429cce151c
Ankur Shahi <ankurshahi@MacBook-Air.phub.net.cable.rogers.com>
2021-07-18 19:05:14 -0400

    comments routes added

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,12 @@
 const sequelize = require('../../config/connection');
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 // get all users
 router.get('/',(req,res)=>{
     console.log('===============');
     Post.findAll({
+        order:[['created_at','DESC']],
         attributes: [
             'id',
             'post_url',
@@ -15,6 +16,15 @@ router.get('/',(req,res)=>{
         ],
         order:[['created_at','DESC']],
         include: [
+            // include the comment model here
+            {
+                model: Comment,
+                attributes:['id','comment_text','post_id','user_id','created_at'],
+                include:{
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model:User,
                 attributes:['username']
@@ -43,6 +53,15 @@ router.get('/:id', (req,res) => {
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),'vote_count']
         ],
         include: [
+             // include the comment model here
+             {
+                model: Comment,
+                attributes:['id','comment_text','post_id','user_id','created_at'],
+                include:{
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes:['username']
