54ca3812d0dcfe55cfd17e9f8def9cd977006bd0
Ankur Shahi <ankurshahi@MacBook-Air.phub.net.cable.rogers.com>
2021-07-24 14:38:43 -0400

    updated user-routes, index and server.js

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers/');
+const sequelize = require('./config/connection');
 
 // set up express app
 const app = express();
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -30,8 +32,9 @@ app.set('view engine','handlebars');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname,'public')));
+app.use(routes);
 
-const routes = require('./controllers/');
+
 // import the connection to sequelize
 
 //turn on connection to db and server
