f2dd8721e13ea8ec378fb03fbc306eec076a9e05
Ankur Shahi <ankurshahi@MacBook-Air.phub.net.cable.rogers.com>
2021-07-24 23:08:18 -0400

    utils - test and helper functions added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-
+const helpers = require('./utils/helpers');
 // const routes = require('./controllers/');
 
 // set up express app
@@ -24,7 +24,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
 
 app.engine('handlebars',hbs.engine);
 app.set('view engine','handlebars');
